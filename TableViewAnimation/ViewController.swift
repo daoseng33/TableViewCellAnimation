6cf540a3235658b0d207d3ce93efc90bd1fe1683
DaoSeng <daoseng33@gmail.com>
2017-09-06 15:29:13 +0800

    Update ViewController.swift

--- TableViewAnimation/ViewController.swift
+++ TableViewAnimation/ViewController.swift
@@ -37,25 +37,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func animateTable() {
         self.tableView.reloadData()
         
-        let cells = self.tableView.visibleCells
         let tableHeight: CGFloat = self.tableView.bounds.size.height
         
-        for i in cells {
-            let cell: UITableViewCell = i as UITableViewCell
+        for cell in self.tableView.visibleCells {
             cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
         }
         
         var index = 0
         
-        for a in cells {
-            let cell: UITableViewCell = a as UITableViewCell
-            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+        for cell in self.tableView.visibleCells {
+            UIView.animate(withDuration: 1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
                 cell.transform = CGAffineTransform(translationX: 0, y: 0);
             }, completion: nil)
             
             index += 1
         }
     }
-    
 }
 
